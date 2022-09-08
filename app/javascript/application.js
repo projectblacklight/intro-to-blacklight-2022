7b8ee687c8d2f87d452e40915a1c7a75a2fb5c9d
Chris Beer <chris@cbeer.info>
2022-09-08 15:22:21 -0700

    Generate Blacklight 8.x into the application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import $ from "jquery"
+import bootstrap from "bootstrap"
+window.bootstrap = bootstrap // Required for Blacklight 7 so it can manage the modals
+window.$ = $ // required as long as blacklight requires jquery
+import "blacklight"
+import dialogPolyfill from "dialog-polyfill"
+Blacklight.onLoad(() => {
+  var dialog = document.querySelector('dialog');
+  dialogPolyfill.registerDialog(dialog);
+})
